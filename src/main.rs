7910ee39299db2aee7af176a7df3af8793eef135
bayge <alex@fluidity.money>
2025-05-11 02:58:33 +0300

    Explicitly not set the lib name, restore abi features

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-#![cfg_attr(target_arch = "wasm32", no_main, no_std)]
+#![cfg_attr(not(any(test, feature = "export-abi")), no_main)]
 
-fn main() {}
+#[cfg(not(any(test, feature = "export-abi")))]
+#[no_mangle]
+pub extern "C" fn main() {}
+
+#[cfg(feature = "export-abi")]
+fn main() {
+    libpurrstream::print_abi("MIT-OR-APACHE-2.0", "pragma solidity ^0.8.23;");
+}
 
 #[panic_handler]
 #[cfg(target_arch = "wasm32")]
